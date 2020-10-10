b4b84e2934b37afa119714e2fe179efda64457fb
Jome Favourite <54733225+jomefavourite@users.noreply.github.com>
2020-10-11 00:05:47 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        divider: "#e7e7e9",
+        color1: "#1d1e35",
+        color2: "#f47c57",
+        color3: "#4a4b5e",
+        color4: "#787887",
+      },
+      borderRadius: {
+        wide: "1.5rem",
+      },
+      fontFamily: {
+        kumbh: "Kumbh Sans",
+      },
+      spacing: {
+        one: "8.4rem",
+        m2: "2.5rem",
+        "-m2": "7rem",
+      },
+      backgroundImage: {
+        img1: "url('../public/images/bg-pattern-desktop.svg')",
+      },
+      backgroundSize: {
+        bg1: "56rem",
+      },
+      backgroundPosition: {
+        bg2: "-450px -290px",
+      },
+      boxShadow: {
+        boxsh: "0px 5px 30px #1d1e3598",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
